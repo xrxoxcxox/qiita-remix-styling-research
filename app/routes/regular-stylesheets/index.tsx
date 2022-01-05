f87645344b7193f0836e1d51de0590cbeab04de9
xrxoxcxox <watanuki@ju-zen.com>
2022-01-05 14:26:33 +0900

    Add global styles to Regular Stylesheets page

--- app/routes/regular-stylesheets/index.tsx
+++ app/routes/regular-stylesheets/index.tsx
@@ -1,7 +1,12 @@
 import styles from '~/styles/global.css'
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:wght@400;700&display=swap" },
+    { rel: "stylesheet", href: styles },
+  ];
 }
 
 export default function RegularStylesheets() {
