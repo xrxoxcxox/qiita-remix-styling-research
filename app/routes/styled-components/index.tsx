6a16f44c3d74ae5215b3504c103f99126cedc6ac
xrxoxcxox <watanuki@ju-zen.com>
2022-01-10 23:48:21 +0900

    Add style to styled-components page

--- app/routes/styled-components/index.tsx
+++ app/routes/styled-components/index.tsx
@@ -1,5 +1,5 @@
+import styled from "styled-components";
 import { GlobalStyle } from "~/components/StyledComponents/Global";
-import { Button } from "~/components/StyledComponents/Button";
 import { Header } from "~/components/StyledComponents/Header";
 import { Footer } from "~/components/StyledComponents/Footer";
 import { Navigation } from "~/components/StyledComponents/Navigation";
@@ -31,18 +31,58 @@ export default function StyledComponents() {
     <>
       <GlobalStyle />
       <Header />
-      <Navigation />
-      {feedItemDataset.map((feedItemData: feedItemDataType) => (
-        <FeedItem
-          data={feedItemData}
-          key={feedItemData.title}
-          className="page-feedItem"
-        />
-      ))}
-      <Ranking dataset={rankingDataset}/>
-      <h1>styled-components</h1>
-      <Button>Button</Button>
-      <Footer />
+      <StyledPageContents>
+        <StyledPageNavigation />
+        <StyledPageMain>
+          <StyledPageMainHeadline>ホーム</StyledPageMainHeadline>
+          {feedItemDataset.map((feedItemData: feedItemDataType) => (
+            <StyledPageFeedItem data={feedItemData} key={feedItemData.title} />
+          ))}
+        </StyledPageMain>
+        <StyledPageSidebar>
+          <Ranking dataset={rankingDataset} />
+          <StyledPageFooter />
+        </StyledPageSidebar>
+      </StyledPageContents>
     </>
   );
 }
+
+const StyledPageContents = styled.div`
+  display: grid;
+  grid-column-gap: 16px;
+  grid-template-areas: "navigation navigation main main main main main main sidebar sidebar sidebar sidebar";
+  grid-template-columns: repeat(12, minmax(48px, 80px));
+  justify-content: center;
+  padding: 24px 16px 48px;
+`;
+
+const StyledPageNavigation = styled(Navigation)`
+  align-self: start;
+  display: flex;
+  flex-direction: column;
+  grid-area: navigation;
+  position: sticky;
+  top: 88px;
+`;
+
+const StyledPageMain = styled.main`
+  grid-area: main;
+`;
+
+const StyledPageMainHeadline = styled.h2`
+  font-size: var(--font-size-subhead);
+  font-weight: bold;
+`;
+
+const StyledPageFeedItem = styled(FeedItem)`
+  margin-top: 16px;
+`;
+
+const StyledPageSidebar = styled.aside`
+  grid-area: sidebar;
+`;
+
+const StyledPageFooter = styled(Footer)`
+  margin-top: 16px;
+`;
