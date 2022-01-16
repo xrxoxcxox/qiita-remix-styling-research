f0d8404b90ef84e6f268bfa2d4eaf9a107465d62
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-17 01:05:43 +0900

    Add style to Tailwind CSS page

--- app/routes/tailwindcss/index.tsx
+++ app/routes/tailwindcss/index.tsx
@@ -1,5 +1,4 @@
 import { Header } from "~/components/TailwindCSS/Header";
-import { Button } from "~/components/TailwindCSS/Button";
 import { Footer } from "~/components/TailwindCSS/Footer";
 import { Navigation } from "~/components/TailwindCSS/Navigation";
 import {
@@ -9,23 +8,33 @@ import {
 import feedItemDataset from "~/datas/dummy-feed-items.json";
 import { Ranking } from "~/components/TailwindCSS/Ranking";
 import rankingDataset from "~/datas/dummy-ranking.json";
+import globalStyles from "~/styles/global.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: globalStyles }];
+}
 
 export default function Tailwind() {
   return (
     <>
       <Header />
-      <Navigation />
-      <h1>Tailwind</h1>
-      {feedItemDataset.map((feedItemData: feedItemDataType) => (
-        <FeedItem
-          data={feedItemData}
-          key={feedItemData.title}
-
-        />
-      ))}
-      <Ranking dataset={rankingDataset} />
-      <Button>Test</Button>
-      <Footer />
+      <div className="grid grid-cols-12 gap-4 justify-center pt-6 px-4 pb-12 w-full max-w-screen-lg mx-auto">
+        <Navigation className="self-start flex flex-col col-start-1 col-end-3" />
+        <main className="col-start-3 col-end-9">
+          <h2 className="text-xl font-bold">ホーム</h2>
+          {feedItemDataset.map((feedItemData: feedItemDataType) => (
+            <FeedItem
+              data={feedItemData}
+              key={feedItemData.title}
+              className="mt-4"
+            />
+          ))}
+        </main>
+        <aside className="col-start-9 col-end-13">
+          <Ranking dataset={rankingDataset} />
+          <Footer className="mt-4" />
+        </aside>
+      </div>
     </>
   );
 }
