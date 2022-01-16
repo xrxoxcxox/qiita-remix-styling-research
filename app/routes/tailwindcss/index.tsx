2b2c1e18aad4beb8e71a214dd051179f8b3045da
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-17 00:42:14 +0900

    Add FeedItem component (Tailwind CSS version)

--- app/routes/tailwindcss/index.tsx
+++ app/routes/tailwindcss/index.tsx
@@ -2,15 +2,27 @@ import { Header } from "~/components/TailwindCSS/Header";
 import { Button } from "~/components/TailwindCSS/Button";
 import { Footer } from "~/components/TailwindCSS/Footer";
 import { Navigation } from "~/components/TailwindCSS/Navigation";
+import {
+  FeedItem,
+  Data as feedItemDataType,
+} from "~/components/TailwindCSS/FeedItem";
+import feedItemDataset from "~/datas/dummy-feed-items.json";
 
 export default function Tailwind() {
   return (
     <>
       <Header />
-      <Navigation/>
+      <Navigation />
       <h1>Tailwind</h1>
+      {feedItemDataset.map((feedItemData: feedItemDataType) => (
+        <FeedItem
+          data={feedItemData}
+          key={feedItemData.title}
+
+        />
+      ))}
       <Button>Test</Button>
-      <Footer/>
+      <Footer />
     </>
   );
 }
