06e44dae0f105abb8ce13d0a1a478f24c5f04280
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-17 00:39:47 +0900

    Move Google Fonts stylesheet settings to app/root.tsx

--- app/routes/regular-stylesheets/index.tsx
+++ app/routes/regular-stylesheets/index.tsx
@@ -26,16 +26,6 @@ import rankingDataset from "~/datas/dummy-ranking.json";
 
 export function links() {
   return [
-    { rel: "preconnect", href: "https://fonts.googleapis.com" },
-    { rel: "preconnect", href: "https://fonts.gstatic.com" },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Noto+Sans:wght@400;700&display=swap",
-    },
-    {
-      rel: "stylesheet",
-      href: "https://fonts.googleapis.com/icon?family=Material+Icons+Round",
-    },
     { rel: "stylesheet", href: globalStyles },
     ...headerLinks(),
     ...navigationLinks(),
