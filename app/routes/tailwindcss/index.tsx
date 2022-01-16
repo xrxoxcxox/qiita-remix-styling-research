891383819fefaa239e018ac73d9f9b85b603acd6
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-16 23:52:46 +0900

    Add Footer component (Tailwind CSS version)

--- app/routes/tailwindcss/index.tsx
+++ app/routes/tailwindcss/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "~/components/TailwindCSS/Header";
 import { Button } from "~/components/TailwindCSS/Button";
+import { Footer } from "~/components/TailwindCSS/Footer";
 
 export default function Tailwind() {
   return (
@@ -7,6 +8,7 @@ export default function Tailwind() {
       <Header />
       <h1>Tailwind</h1>
       <Button>Test</Button>
+      <Footer/>
     </>
   );
 }
