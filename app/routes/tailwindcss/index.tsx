fc30ed16e9bd1a6a6ae94c75efa6b8792e4f0465
Keisuke Watanuki <watanuki@ju-zen.com>
2022-01-16 23:14:45 +0900

    Add Header component (Tailwind CSS version)

--- app/routes/tailwindcss/index.tsx
+++ app/routes/tailwindcss/index.tsx
@@ -1,8 +1,10 @@
+import { Header } from "~/components/TailwindCSS/Header";
 import { Button } from "~/components/TailwindCSS/Button";
 
 export default function Tailwind() {
   return (
     <>
+      <Header />
       <h1>Tailwind</h1>
       <Button>Test</Button>
     </>
